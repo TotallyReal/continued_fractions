04efd371ed8a420844aaa45da5760016ebc42602
Ofir <eofirdavid@gmail.com>
2023-11-01 17:40:17 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='continued_fractions',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Ofir David',
+    author_email='eofirdavid@gmail.com',
+    description='Continued fractions and Zaremba conjecture'
+)
